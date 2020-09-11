3d2afabe18275aab11c615e7a6e9aadbc7f7256b
kinjaldand <kinjal_dand@yahoo.in>
2020-09-11 21:54:20 +0530

    Update ReduceIntensityLevel.py

--- Week1/ReduceIntensityLevel.py
+++ Week1/ReduceIntensityLevel.py
@@ -1,2 +1,15 @@
-import PIL
+from PIL import Image
 import numpy as np
+
+def reduceIntensity(img_path):
+  img = Image.open(img_path)
+  pix = np.array(img)
+  initial_level = 2
+  end_level = 256
+  levels = []
+  while (initial_level<=end_level):
+    levels.append(initial_level)
+    initial_level = initial_level*2
+    
+  print(levels)
+    
